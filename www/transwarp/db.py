c62f4b90068a26da9d1e8a9e7339fdb5f1096d9d
Jerry <zlfeng_2012@163.com>
2018-04-27 00:48:24 +0800

    -add package transwarp

---
+++ www/transwarp/db.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+
+
+def create_engine(user, password, host, port, database):
+    pass
+
+
+def select(sql, *args):
+    return 0
+
+def update(sql, *args):
+    pass
+
+def insert(sql, *args):
+    pass
+
+def delete(sql, *args):
+    pass
