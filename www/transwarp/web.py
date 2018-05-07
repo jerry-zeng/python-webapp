05da1d6d1e6a44fdd7959bfac056fd0291c38480
Jerry <zlfeng_2012@163.com>
2018-05-07 19:16:02 +0800

    -添加框架代码

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -2,12 +2,158 @@
 
 import threading
 from utils import Dict
+import os, sys, time, datetime, functools
+import re
 
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
 
 ctx = threading.local()
 
+
+# 状态码.
+_RESPONSE_STATUSES = {
+    # Informational
+    100: 'Continue',
+    101: 'Switching Protocols',
+    102: 'Processing',
+
+    # Successful
+    200: 'OK',
+    201: 'Created',
+    202: 'Accepted',
+    203: 'Non-Authoritative Information',
+    204: 'No Content',
+    205: 'Reset Content',
+    206: 'Partial Content',
+    207: 'Multi Status',
+    226: 'IM Used',
+
+    # Redirection
+    300: 'Multiple Choices',
+    301: 'Moved Permanently',
+    302: 'Found',
+    303: 'See Other',
+    304: 'Not Modified',
+    305: 'Use Proxy',
+    307: 'Temporary Redirect',
+
+    # Client Error
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    402: 'Payment Required',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    407: 'Proxy Authentication Required',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    412: 'Precondition Failed',
+    413: 'Request Entity Too Large',
+    414: 'Request URI Too Long',
+    415: 'Unsupported Media Type',
+    416: 'Requested Range Not Satisfiable',
+    417: 'Expectation Failed',
+    418: "I'm a teapot",
+    422: 'Unprocessable Entity',
+    423: 'Locked',
+    424: 'Failed Dependency',
+    426: 'Upgrade Required',
+
+    # Server Error
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
+    505: 'HTTP Version Not Supported',
+    507: 'Insufficient Storage',
+    510: 'Not Extended',
+}
+
+_REG_RESPONSE_STATUSES = re.compile(r"\d\d\d(\ [\w ]+)?$")
+
+_RESPONSE_HEADERS = (
+    'Accept-Ranges',
+    'Age',
+    'Allow',
+    'Cache-Control',
+    'Connection',
+    'Content-Encoding',
+    'Content-Language',
+    'Content-Length',
+    'Content-Location',
+    'Content-MD5',
+    'Content-Disposition',
+    'Content-Range',
+    'Content-Type',
+    'Date',
+    'ETag',
+    'Expires',
+    'Last-Modified',
+    'Link',
+    'Location',
+    'P3P',
+    'Pragma',
+    'Proxy-Authenticate',
+    'Refresh',
+    'Retry-After',
+    'Server',
+    'Set-Cookie',
+    'Strict-Transport-Security',
+    'Trailer',
+    'Transfer-Encoding',
+    'Vary',
+    'Via',
+    'Warning',
+    'WWW-Authenticate',
+    'X-Frame-Options',
+    'X-XSS-Protection',
+    'X-Content-Type-Options',
+    'X-Forwarded-Proto',
+    'X-Powered-By',
+    'X-UA-Compatible',
+)
+
+_RESPONSE_HEADERS_DICT = dict( zip(map(lambda x:x.upper(), _RESPONSE_HEADERS), _RESPONSE_HEADERS) )
+
+_HEADER_X_POWERED_BY = ('X-Powered-By', 'transwarp/1.0')
+
 class HttpError(Exception):
-    pass
+    def __init__(self, code):
+        super(Exception, self).__init__()
+        self.status = "%d %s" % (code, _RESPONSE_STATUSES[code])
+
+    def header(self, key, value):
+        if not hasattr(self, "_headers"):
+            self._headers = [_HEADER_X_POWERED_BY]
+        self._headers.append( (key, value) )
+
+    @property
+    def headers(self):
+        if hasattr(self, "_headers"):
+            return self._headers
+        return []
+
+    def __str__(self):
+        return self.status
+
+    __repr__ = __str__
+
+class RedirectError(HttpError):
+    def __init__(self, code, location):
+        super(RedirectError, self).__init__(code)
+        self.location = location
+
+    def __str__(self):
+        return "%s, %s" % (self.status, self.location)
+
+    __repr__ = __str__
 
 
 class Request(object):
