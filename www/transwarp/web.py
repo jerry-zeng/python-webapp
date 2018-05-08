5c6512f995d938cb841348ee91db52615b55bbf7
Jerry <zlfeng_2012@163.com>
2018-05-08 19:21:04 +0800

    -完成class Request

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -3,7 +3,7 @@
 import threading
 from utils import Dict
 import os, sys, time, datetime, functools
-import re
+import re, urllib, cgi
 
 try:
     from cStringIO import StringIO
@@ -12,6 +12,7 @@ except:
 
 ctx = threading.local()
 
+#---------------------------------------------------------------------
 
 # 状态码.
 _RESPONSE_STATUSES = {
@@ -155,24 +156,192 @@ class RedirectError(HttpError):
 
     __repr__ = __str__
 
+def badRequest():
+    return HttpError(400)
+def unauthorized():
+    return HttpError(401)
+def forbidden():
+    return HttpError(403)
+def notFound():
+    return HttpError(404)
+def conflict():
+    return HttpError(409)
+def internalError():
+    return HttpError(500)
+def redirect(location):
+    return RedirectError(301, location)
+def found(location):
+    return RedirectError(302, location)
+def seeOther(location):
+    return RedirectError(303, location)
+
+#---------------------------------------------------------------------
+DEFAULT_ENCODING = "utf-8"
+
+def _to_str(s):
+    if isinstance(s, str):
+        return s
+    if isinstance(s, unicode):
+        return s.encode(DEFAULT_ENCODING)
+    return str(s)
+
+def _to_unicode(s, encoding=DEFAULT_ENCODING):
+    return s.encode(encoding)
+
+def _quote(s, encoding=DEFAULT_ENCODING):
+    if isinstance(s, unicode):
+        s = s.encode(encoding)
+    return urllib.quote(s)
+
+def _unquote(s, encoding=DEFAULT_ENCODING):
+    s = urllib.unquote(s)
+    return s.decode(encoding)
+
+
+
+def get(path):
+    def decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = "GET"
+        return func
+    return decorator
+
+def post(path):
+    def decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = "POST"
+        return func
+    return decorator
+
+#----------------------------------Request & Response-----------------------------------
+
+class MultipartFile(object):
+    def __init__(self, storage):
+        self.filename = _to_unicode(storage.filename)
+        self.file = storage.file
+
 
 class Request(object):
-    def input(self):
-        pass
+    def __init__(self, environ):
+        self._environ = environ
 
-    def get(self, key, default=None):
-        pass
+    def _parse_input(self):
+        def _convert(item):
+            if isinstance(item, list):
+                return [_to_unicode(i.value) for i in item]
+            if item.filename:
+                return MultipartFile(item)
+            return _to_unicode(item)
+
+        fs = cgi.FieldStorage(fp=self._environ.get("wsgi.input"), environ=self._environ, keep_blank_values=True)
+        inputs = dict()
+        for key in fs:
+            inputs[key] = _convert(fs[key])
+        return inputs
+
+    def _get_raw_input(self):
+        if not hasattr(self, "_raw_input"):
+            self._raw_input = self._parse_input()
+        return self._raw_input
+
+    def __getitem__(self, key):
+        r = self._get_raw_input()[key]
+        if isinstance(r, list):
+            return r[0]
+        return r
+
+    @property
+    def environ(self):
+        return self._environ
 
     @property
     def path_info(self):
-        pass
+        return urllib.unquote(self._environ.get("PATH_INFO", ""))
 
     @property
-    def header(self):
-        pass
+    def host(self):
+        return self._environ.get("HTTP_HOST", "")
+
+    @property
+    def remote_addr(self):
+        return self._environ.get("REMOTE_ADDR", "0.0.0.0")
+
+    @property
+    def document_root(self):
+        return self._environ.get("DOCUMENT_ROOT", "")
+
+    @property
+    def query_string(self):
+        return self._environ.get("QUERY_STRING", "")
+
+    @property
+    def request_method(self):
+        return self._environ.get("REQUEST_METHOD", "")
+
+    @property
+    def headers(self):
+        return Dict(**self._get_header())
+
+    @property
+    def cookies(self):
+        return Dict(**self._get_cookie())
+
+    def input(self, **kw):
+        copy = Dict(**kw)
+        r = self._get_raw_input()
+        for k,v in r.iteritems():
+            copy[k] = v[0] if isinstance(v, list) else v
+        return copy
+
+    def get(self, key, default=None):
+        r = self._get_raw_input().get(key, default)
+        if isinstance(r, list):
+            return r[0]
+        return r
+
+    def gets(self, key):
+        r = self._get_raw_input()[key]
+        if isinstance(r, list):
+            return r[:]
+        return [r]
+
+    def get_body(self):
+        fp = self._environ.get("wsgi.input")
+        return fp.read()
+
+
+    def _get_header(self):
+        if not hasattr(self, "_headers"):
+            kvs = {}
+            self._headers = kvs
+
+            for k,v in self._environ.iteritems():
+                if k.startsWith("HTTP_"):
+                    kvs[ k[5:].replace("_", "-").upper() ] = v.decode(DEFAULT_ENCODING)
+
+        return self._headers
+
+    def header(self, key, default=None):
+        d = self._get_header()
+        return d.get(key.upper(), default)
+
+    def _get_cookie(self):
+        if not hasattr(self, "_cookies"):
+            kvs = {}
+            self._cookies = kvs
+
+            cookieStr = self._environ.get("HTTP_COOKIE")
+            if cookieStr:
+                for v in cookieStr.splite(";"):
+                    pos = v.find("=")
+                    if pos > 0:
+                        kvs[ v[:pos].strip() ] = _unquote( v[pos+1:] )
+
+        return self._cookies
 
     def cookie(self, key, default=None):
-        pass
+        d = self._get_cookie()
+        return d.get(key.upper(), default)
 
 
 class Response(object):
@@ -190,25 +359,24 @@ class Response(object):
     def status(self, value):
         pass
 
-
+#----------------------------------View-----------------------------------
 class TemplateEngine(object):
     pass
 
 
-def get(path):
+class Jinja2TemplateEngine(TemplateEngine):
     pass
 
-def post(path):
-    pass
 
 def view(path):
     pass
 
+#-------------------------------Interceptor---------------------------------
 def interceptor(pattern):
     pass
 
-class Jinja2TemplateEngine(TemplateEngine):
-    pass
+
+#---------------------------------------------------------------------
 
 class WSGIApplication(object):
     def __init__(self, document_root=None, **kw):
