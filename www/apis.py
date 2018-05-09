bc0eec3cfb6119f70acdd98d091ee90b6916a8aa
Jerry <zlfeng_2012@163.com>
2018-05-10 02:27:06 +0800

    -添加 apis.py

---
+++ www/apis.py
@@ -0,0 +1,45 @@
+#coding=utf-8
+
+import functools, json, logging
+from transwarp.web import ctx
+
+
+class ApiError(StandardError):
+    def __init__(self, err, data="", message=""):
+        super(ApiError, self).__init__(message)
+        self.error = err
+        self.data = data
+        self.message = message
+
+class ApiValueError(ApiError):
+    def __init__(self, field, msg):
+        super(ApiValueError, self).__init__("value invalid", field, msg)
+
+class ApiResourceNotFoundError(ApiError):
+    def __init__(self, field, msg):
+        super(ApiValueError, self).__init__("resource not found", field, msg)
+
+class ApiPermissionError(ApiError):
+    def __init__(self, msg):
+        super(ApiValueError, self).__init__("permission denied", "permission", msg)
+
+
+def api(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        try:
+            r = func(*args, **kw)
+            r = json.dumps( r )
+        except ApiError, e:
+            r = json.dumps(dict(error=e.error, data=e.data, message=e.message))
+        except Exception, e:
+            logging.exception(e)
+            r = json.dumps(dict(error="internalError", data=e.__class__.__name__, message=e.message))
+
+        ctx.response.content_type = "application/json"
+        return r
+    return wrapper
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- www/urls.py
+++ www/urls.py
@@ -1,15 +1,19 @@
 #coding=utf-8
 
+import os, re, time, base64, hashlib, logging
+
 from transwarp.web import get, post, view
 from models import User, Blog, Comment
+from apis import api, ApiError, ApiValueError
 
+#------------------------------test------------------------------
 # @view("test_users.html")
 # @get("/")
 # def test_users():
 #     users = User.find_all()
 #     return dict(users=users)
 
-
+#------------------------------Logic------------------------------
 @view("blogs.html")
 @get("/")
 def index():
@@ -18,4 +22,17 @@ def index():
     return dict(blogs=blogs, user=user)
 
 
+#------------------------------Api------------------------------
+@api
+@get("/api/users")
+def api_get_users():
+    users = User.find_by("order by created_at desc")
+    for u in users:
+        u.password = "******"
+    return dict(users=users)
 
+@api
+@get("/api/blogs")
+def api_get_blogs():
+    blogs = Blog.find_by("order by created_at desc")
+    return dict(blogs=blogs)
